518147f0b678135ee6d04a6c361898e61eb66841
Nick Mykins <nick.mykins@gmail.com>
2015-01-19 16:43:02 -0500

    cleanup unused functions in deploy.py

--- deploy.py
+++ deploy.py
@@ -4,29 +4,8 @@ import os
 import arrow
 from cowpost import db_connect
 PATH = os.getcwd()
-COWPOSTPATH = PATH + '/cowposts'
-COWDATE_FMT = 'YYYY-MM-DD HH:mm:ss'
 template_loader = jinja2.FileSystemLoader(searchpath=PATH)
 template_env = jinja2.Environment(loader=template_loader)
-
-
-def make_cowdict(cownamedlines):
-    cowdate = lambda moo: moo[0].strip()
-    cowtext = lambda moo: ''.join(moo[1:])
-    return {cowdate(cownamedpost[1]): 
-               (cownamedpost[0], cowtext(cownamedpost[1])) 
-           for cownamedpost in cownamedlines}
-
-
-def cowblogsort(cowdict):
-    cowarrows = [arrow.get(cowdate, COWDATE_FMT)
-                 for cowdate in cowdict]
-    cowblogorder = [moo.format(COWDATE_FMT) 
-                    for moo in sorted(cowarrows)[::-1]]
-    return [{'htmltitle': cowdict[cowdate][0].replace('cowpost','html'), 
-             'date': cowdate, 
-             'cowtext': cowdict[cowdate][1]}
-            for cowdate in cowblogorder]
 
 
 def write_cowfiles(cowposts):
