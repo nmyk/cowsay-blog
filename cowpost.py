1743b491bea6b26252966129d93d35921cc0cacd
Nick Mykins <nick.mykins@gmail.com>
2015-01-19 16:43:30 -0500

    track cowpost.py

---
+++ cowpost.py
@@ -0,0 +1,56 @@
+import sqlite3
+import arrow
+from sh import cowsay
+import sys
+import os
+PATH = os.getcwd()
+
+def db_connect():
+    conn = sqlite3.connect('cowposts.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def db_init():
+    with db_connect() as conn:
+        cur = conn.cursor()
+        cur.execute('''
+            create table if not exists
+            cowposts (
+            id integer primary key autoincrement,
+            title text not null,
+            date text not null,
+            text text not null,
+            cowtext text not null)
+        ''')
+        conn.commit()
+        
+
+def db_insert(title, text):
+    values = dict(
+        date=arrow.now().format('YYYY-MM-DD HH:mm:ss'),
+        title=title,
+        text=text,
+        cowtext=str(cowsay(text)))
+    with db_connect() as conn:
+        cur = conn.cursor()
+        cur.execute('''
+            insert into cowposts
+            (date, title, text, cowtext)
+            values
+            (:date, :title, :text, :cowtext)
+        ''', values)
+        conn.commit()
+
+
+def main():
+	if not os.path.isfile(PATH + 'cowposts.db'):
+		db_init()
+	filename = sys.argv[1]
+	with open(filename, 'r') as f:
+		text = f.read()
+	db_insert(filename, text)
+
+
+if __name__ == '__main__':
+	main()
